2bcb513be4ea84a13a3c6108d98966373b95a914
Ayatooo <louisreynard919@gmail.com>
2023-06-30 16:05:46 +0200

    :zap: done

--- script.sh
+++ script.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
 
+# Vérifier si l'URL GitHub a été fournie en argument
+if [ -z "$1" ]; then
+  echo "Veuillez fournir l'URL GitHub du projet."
+  exit 1
+fi
+
+# Cloner le projet depuis l'URL GitHub fournie
+git clone "$1" myproject
+cd myproject
+
 # Vérifier les dépendances système
 if ! command -v php >/dev/null 2>&1; then
-    echo "PHP n'est pas installé. Veuillez installer PHP 8+."
-    exit 1
+  echo "PHP n'est pas installé. Veuillez installer PHP 8+."
+  exit 1
 fi
 
 if ! command -v docker >/dev/null 2>&1; then
-    echo "Docker n'est pas installé. Veuillez installer Docker."
-    exit 1
+  echo "Docker n'est pas installé. Veuillez installer Docker."
+  exit 1
 fi
 
 if ! command -v composer >/dev/null 2>&1; then
-    echo "Composer n'est pas installé. Veuillez installer Composer 2.0+."
-    exit 1
+  echo "Composer n'est pas installé. Veuillez installer Composer 2.0+."
+  exit 1
 fi
 
 if ! command -v node >/dev/null 2>&1; then
-    echo "Node.js n'est pas installé. Veuillez installer Node.js v17+."
-    exit 1
+  echo "Node.js n'est pas installé. Veuillez installer Node.js v17+."
+  exit 1
 fi
-
-# Demander l'URL GitHub du projet à cloner
-read -p "Veuillez entrer l'URL GitHub du projet : " github_url
-
-# Cloner le projet depuis l'URL GitHub fournie
-git clone "$github_url" myproject
-cd myproject
 
 # Installer les dépendances avec Composer
 docker run --rm --interactive --tty \
-    --volume $PWD:/app \
-    composer install --ignore-platform-reqs
+  --volume $PWD:/app \
+  composer install --ignore-platform-reqs
 
 # Copier le fichier .env.example
 cp .env.example .env
